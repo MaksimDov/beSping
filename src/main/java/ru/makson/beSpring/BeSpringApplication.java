ee76dbd91c020ad90634fd3eab34f4412a41b570
MaksimDov <maksimdovydenko@mail.ru>
2020-08-17 21:05:38 +0700

    Init commit!

---
+++ src/main/java/ru/makson/beSpring/BeSpringApplication.java
@@ -0,0 +1,14 @@
+package ru.makson.beSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
